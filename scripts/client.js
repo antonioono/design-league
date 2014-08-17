ba4fe325d91462a6ca76d8b7805711d836d1c7ea
Antonio Ono <Antonio@Antonios-MacBook-Pro.local>
2014-08-17 14:44:20 -0700

    Better scrolling/rollover behavior

--- scripts/client.js
+++ scripts/client.js
@@ -1,27 +1,56 @@
 $(document).ready(function(){
-    var container   = $("body > header, body > nav"),
-        indexContainer = $("body.index > header, body.index > nav");
-        a           = $("body > header a, body > nav a");
+    var header      = $("body > header, body > nav"),
+        headerIndex = $("body.index > header, body.index > nav"),
+        headerLink  = $("body > header a, body > nav a"),
+        nav         = $("body > nav"),
+        content     = $("body > .content"),
+        article     = $("body > .content > article");
         
-    indexContainer.addClass("rollover");
+    /* setBackground(); */
     
-    a.mouseenter(function() {
-        container.addClass("rollover");
-        $("body > .content").addClass("inactive");
+    /*
+function setBackground() {
+        var imageSrc    = $("body > .content > article .body img").first().attr("src");
+        $("body").css("background-image", "url('" + imageSrc + "');");
+        console.log(imageSrc);
+    }*/
+
+    focusHeader();
+        
+    /*
+setTimeout(function() {
+        focusContent();
+    }, 4000);
+*/
+    
+    headerLink.mouseenter(function() {
+        focusHeader();
     });
     
-    a.mouseout(function() {
-        container.removeClass("rollover");
-        $("body > .content").removeClass("inactive");
+    headerLink.mouseout(function() {
+        focusContent();
     });
     
     $(document).on("scroll", function(){
-        if ($(document).scrollTop() > 100) {
-            indexContainer.removeClass("rollover");
-            $("body.index > .content").removeClass("inactive");
-        } else {
-            indexContainer.addClass("rollover");
-            $("body.index > .content").removeClass("inactive");
+        if ($(document).scrollTop() > 0) { // Not at top
+            focusContent();
+        } else { // At top
+            focusHeader();
         }
     });
+    
+    article.mouseenter(function() {
+        focusContent();
+    });
+    
+    function focusContent() {
+        header.removeClass("rollover");
+        content.removeClass("inactive");
+    }
+    
+    function focusHeader() {
+        header.addClass("rollover");
+        content.addClass("inactive");
+    }
+    
 });
