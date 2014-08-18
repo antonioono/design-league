0b94c3bc0d192a6eb142b8de6877954f7dfcb75f
Antonio Ono <Antonio@Antonios-MacBook-Pro.local>
2014-08-17 17:08:10 -0700

    Fixed dynamic background image; Minor type tweaks

--- scripts/client.js
+++ scripts/client.js
@@ -4,53 +4,89 @@ $(document).ready(function(){
         headerLink  = $("body > header a, body > nav a"),
         nav         = $("body > nav"),
         content     = $("body > .content"),
-        article     = $("body > .content > article");
+        article     = $("body > .content > article"),
+        bg          = $("body .bg"),
+        isIndex     = $("body").hasClass("index"),
+        isPost      = !isIndex;
         
-    /* setBackground(); */
+    setBackground();
     
-    /*
-function setBackground() {
+    function setBackground() {
         var imageSrc    = $("body > .content > article .body img").first().attr("src");
-        $("body").css("background-image", "url('" + imageSrc + "');");
-        console.log(imageSrc);
-    }*/
+        bg.css("background-image", "url('"+imageSrc+"')");
+        console.log("url('" + imageSrc + "');");
+    }
 
     focusHeader();
-        
-    /*
-setTimeout(function() {
+    
+    if (isIndex) content.removeClass("inactive");
+       
+    setTimeout(function() {
         focusContent();
-    }, 4000);
-*/
+    }, 3000);
     
     headerLink.mouseenter(function() {
-        focusHeader();
+        if (isPost) {
+            focusHeader();
+        } else { // Index
+            if ($(document).scrollTop() < 1) { // At top
+                focusBoth()
+            } else { // Not at top, default behavior
+                focusHeader();
+            }
+        }
     });
     
     headerLink.mouseout(function() {
-        focusContent();
+        if (isPost) {
+            focusContent();
+        } else { // Index
+            if ($(document).scrollTop() < 1) { // At top
+                focusBoth();
+            } else { // Not at top, default behavior
+                focusContent();
+            }
+        }
     });
     
+    article.mouseenter(function() {
+        if (isPost) {
+            focusContent();
+            console.log("ISPOST");
+        } else {
+            if ($(document).scrollTop() < 1) { // At top
+                focusBoth();
+            } else { // Not at top, default behavior
+                focusContent()
+            }
+        }
+    });
+    
+
     $(document).on("scroll", function(){
         if ($(document).scrollTop() > 0) { // Not at top
             focusContent();
         } else { // At top
             focusHeader();
+            if (isIndex) content.removeClass("inactive");
         }
-    });
-    
-    article.mouseenter(function() {
-        focusContent();
     });
     
     function focusContent() {
         header.removeClass("rollover");
         content.removeClass("inactive");
+        bg.addClass("muted");
     }
     
     function focusHeader() {
         header.addClass("rollover");
         content.addClass("inactive");
+        bg.removeClass("muted");
+    }
+    
+    function focusBoth() {
+        header.addClass("rollover");
+        content.removeClass("inactive");
     }
     
 });
